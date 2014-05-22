ae73c4de9447aa16310d72178047b084d3ddfdf2
Tom Oberhauser <tom@devfoo.de>
2014-05-22 18:42:48 +0200

    Aufgabe 5 - neue leere View MailListView hinzugefügt.

---
+++ de.bht.fpa.mail.s798158.maillist/src/de/bht/fpa/mail/s798158/maillist/MailListView.java
@@ -0,0 +1,20 @@
+package de.bht.fpa.mail.s798158.maillist;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class MailListView extends ViewPart {
+
+  @Override
+  public void createPartControl(Composite parent) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void setFocus() {
+    // TODO Auto-generated method stub
+
+  }
+
+}
