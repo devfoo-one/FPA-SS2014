7adef90991a4b2546627093b789fc95a564d2b37
Tom Oberhauser <tom@devfoo.de>
2014-06-10 00:53:38 +0200

    Beginn Aufgabe 7 , leere Klassen eingefügt.

---
+++ de.bht.fpa.mail.s798158.filter/src/de/bht/fpa/mail/s798158/filter/Main.java
@@ -0,0 +1,10 @@
+package de.bht.fpa.mail.s798158.filter;
+
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Testtool für die Filter");
+
+  }
+
+}
---
+++ de.bht.fpa.mail.s798158.filter/src/de/bht/fpa/mail/s798158/filter/Read.java
@@ -0,0 +1,16 @@
+package de.bht.fpa.mail.s798158.filter;
+
+import java.util.Set;
+
+import de.bht.fpa.mail.s000000.common.filter.IFilter;
+import de.bht.fpa.mail.s000000.common.mail.model.Message;
+
+public class Read implements IFilter {
+
+  @Override
+  public Set<Message> filter(Iterable<Message> messagesToFilter) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
