977f3526fcec4d594c0a0ab59459896a8c8ccfac
Tom Oberhauser <tom@devfoo.de>
2014-06-17 23:59:55 +0200

    Statusbar-Plugin funktional.

--- de.bht.fpa.mail.s798158.main/src/de/bht/fpa/mail/s798158/main/ApplicationWorkbenchWindowAdvisor.java
+++ de.bht.fpa.mail.s798158.main/src/de/bht/fpa/mail/s798158/main/ApplicationWorkbenchWindowAdvisor.java
@@ -25,7 +25,7 @@ public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
     IWorkbenchWindowConfigurer configurer = getWindowConfigurer();
     configurer.setInitialSize(new Point(WINDOW_WIDTH, WINDOW_HEIGHT));
     configurer.setShowCoolBar(false);
-    configurer.setShowStatusLine(false);
+    configurer.setShowStatusLine(true);
     configurer.setTitle("FPA Mailer"); //$NON-NLS-1$
   }
 }
---
+++ de.bht.fpa.mail.s798158.statusbar/src/de/bht/fpa/mail/s798158/statusbar/StatusBar.java
@@ -0,0 +1,41 @@
+package de.bht.fpa.mail.s798158.statusbar;
+
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.ISelectionListener;
+import org.eclipse.ui.IStartup;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+
+import de.bht.fpa.mail.s000000.common.rcp.statusbar.StatusBarHelper;
+import de.bht.fpa.mail.s798158.fsnavigation.MyFileSystemObject;
+import de.bht.fpa.mail.s798158.fsnavigation.SelectionHelper;
+
+public class StatusBar implements IStartup {
+
+  @Override
+  public void earlyStartup() {
+    // http://help.eclipse.org/helios/index.jsp?topic=%2Forg.eclipse.platform.doc.isv%2Freference%2Fapi%2Forg%2Feclipse%2Fui%2FIStartup.html
+    final IWorkbench workbench = PlatformUI.getWorkbench();
+    workbench.getDisplay().asyncExec(new Runnable() {
+      @Override
+      public void run() {
+        IWorkbenchWindow window = workbench.getActiveWorkbenchWindow();
+        if (window != null) {
+          window.getActivePage().addSelectionListener(new ISelectionListener() {
+            @Override
+            public void selectionChanged(IWorkbenchPart part, ISelection selection) {
+              MyFileSystemObject selectedFSO = SelectionHelper.handleStructuredSelection(selection,
+                  MyFileSystemObject.class);
+              if (selectedFSO != null && selectedFSO instanceof MyFileSystemObject) {
+                final String message = "Directory '" + selectedFSO.getFile().getAbsolutePath() + "' was selected";
+                StatusBarHelper.setMessage(message);
+              }
+            }
+          });
+        }
+      }
+    });
+  }
+}
